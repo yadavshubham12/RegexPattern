f4d0dc810e163e67d3a8bd464052a46976ed4a05
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-31 19:31:39 +0530

    ADDED: Password should have 1 Special Character

--- src/main/java/com/blz/Regexp/PasswordValidator.java
+++ src/main/java/com/blz/Regexp/PasswordValidator.java
@@ -8,27 +8,27 @@ public class PasswordValidator {
 
     private static final String RULE_UPPERCASE = ".*[A-Z].*";
     private static final String RULE_NUMERIC = ".*[0-9].*";
+    private static final String RULE_SPECIAL = ".*[^A-Za-z0-9].*";
     private static final int MIN_LENGTH = 8;
 
     public static boolean isValid(String password) {
         if (password.length() < MIN_LENGTH) {
             return false;
         }
-
         Pattern patternUppercase = Pattern.compile(RULE_UPPERCASE);
         Matcher matcherUppercase = patternUppercase.matcher(password);
-
         Pattern patternNumeric = Pattern.compile(RULE_NUMERIC);
         Matcher matcherNumeric = patternNumeric.matcher(password);
+        Pattern patternSpecial = Pattern.compile(RULE_SPECIAL);
+        Matcher matcherSpecial = patternSpecial.matcher(password);
 
-        return matcherUppercase.matches() && matcherNumeric.matches();
+        return matcherUppercase.matches() && matcherNumeric.matches() && matcherSpecial.matches();
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter password: ");
         String password = scanner.nextLine();
-
         if (isValid(password)) {
             System.out.println("Password is valid.");
         } else {
