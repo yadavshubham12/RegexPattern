f3105bc5209bc5127b87fef5a3a7854adc01bc94
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-02-04 21:25:10 +0530

    ADDED: Lambda Expressions to user entry

--- src/main/java/com/blz/Regexp/UserValidationTest.java
+++ src/main/java/com/blz/Regexp/UserValidationTest.java
@@ -1,34 +1,38 @@
 package com.blz.Regexp;
 
-import org.testng.annotations.Test;
-import org.junit.runners.Parameterized;
-import java.util.Arrays;
-import java.util.Collection;
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-    public class EmailValidationTest {
+import java.util.Scanner;
+import java.util.function.Predicate;
 
-        private String email;
-        private boolean expectedResult;
+public class UserValidationTest {
+    private static Scanner sc = new Scanner(System.in);
 
-        public EmailValidationTest(String email, boolean expectedResult) {
-            this.email = email;
-            this.expectedResult = expectedResult;
-        }
-        public static Collection<Object[]> data() {
-            return Arrays.asList(new Object[][] {
-                    {"shubham.yadav@example.com", true},
-                    {"shubham_yadav@example.com", true},
-                    {"shubhamyadav@sub.example.com", true},
-                    {"shubhamyadav@example", false},
-                    {"shubhamyadavexample.com", false},
-                    {"@example.com", false}
-            });
-        }
-        @Test
-        public void testEmailValidation() {
-            boolean result = User.isValidEmail(email);
-            assertEquals(expectedResult, result);
+    public static void main(String[] args) {
+        System.out.print("Enter first name: ");
+        String firstName = sc.nextLine();
+        validateInput(firstName, (s) -> s.matches("[A-Z][a-zA-Z]*"));
+
+        System.out.print("Enter last name: ");
+        String lastName = sc.nextLine();
+        validateInput(lastName, (s) -> s.matches("[a-zA-Z]+([ '-][a-zA-Z]+)*"));
+
+        System.out.print("Enter email: ");
+        String email = sc.nextLine();
+        validateInput(email, (s) -> s.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}"));
+
+        System.out.print("Enter mobile: ");
+        String mobile = sc.nextLine();
+        validateInput(mobile, (s) -> s.matches("[0-9]{10}"));
+
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+        validateInput(password, (s) -> s.matches("(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{6,}"));
+    }
+
+    private static void validateInput(String input, Predicate<String> validationRule) {
+        if (validationRule.test(input)) {
+            System.out.println("Valid");
+        } else {
+            System.out.println("Invalid");
         }
     }
+}
